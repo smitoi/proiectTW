ed0dcddca0ae5412b51ce037310eb9442862180f
smitoi <smitoi@users.noreply.github.com>
2020-04-24 01:58:48 +0300

    update quiz
    responsive quiz + updated readme

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # Proiect pentru laboratorul de Tehnici Web din cadrul FMI - UniBuc
 
-Am realizat site-ul unei școli auto fictive din București. Site-ul prezintă dotările și personalul școlii. De asemenea, ~~există~~ va exista o aplicație JavaScript ce permite utilizatorilor să susțină teste de legislație simulate. Site-ul poate fi văzut [aici](https://smitoi.github.io/proiectTW)
+Am realizat site-ul unei școli auto fictive din București. Site-ul prezintă dotările și personalul școlii. De asemenea, există o aplicație JavaScript ce permite utilizatorilor să susțină teste de legislație simulate (deocamdată ele au formatul de "Întrebare <listă răspunsuri corecte>" pentru testare. Site-ul poate fi văzut [aici](https://smitoi.github.io/proiectTW)
 
 Toate datele sunt fictive, toate detaliile care pot părea personale (numere de telefon, nume, adrese) fiind alese aleator. Imaginile sunt preluate de pe Google Images.
+
+
+
+## GOALS:
+
+- [x] Basic HTML / CSS
+
+- [x] Make the HTML / CSS repsonsive + CSS animation
+
+- [x] Dropdown menu with JavaScript
+
+- [x] Quiz app with JavaScript
+
+- [x] Make quiz app responsive + add support for questions with images
+
+- [x] Add timer for quiz
+
+- [ ] Read questions from .JSON + shuffle them
+
+- [ ] Add support for additional answers
+
+- [ ] NodeJS (maybe a login system with keeping track of quizes)
+
+- [ ] Make the code a little more readable
--- js/quiz.js
+++ js/quiz.js
@@ -7,14 +7,18 @@ var questions_corect;
 var questions_wrong;
 var question_index;
 var answers_selected;
-var maxim_wrong
+var maxim_wrong;
 var delayMs;
+var contor;
+var timp;
+var minute;
+var secunde;
 
 function prepare_quiz(optiune) {
     questions = [
       ['Întrebare 1', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [1]],
       ['Întrebare 2', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [2]],
-      ['Întrebare 3', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [3]],
+      ['Întrebare 3', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [3], '../images/street1.jpg'],
       ['Întrebare 1 2', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [1, 2]],
       ['Întrebare 1 3', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [1, 3]],
       ['Întrebare 2 3', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [2, 3]],
@@ -26,7 +30,7 @@ function prepare_quiz(optiune) {
       ['Întrebare 1 3', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [1, 3]],
       ['Întrebare 2 3', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [2, 3]],
       ['Întrebare 1 2 3', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [1, 2, 3]],
-      ['Întrebare 1', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [1]],
+      ['Întrebare 1', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [1], '../images/street2.jpg'],
       ['Întrebare 2', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [2]],
       ['Întrebare 3', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [3]],
       ['Întrebare 1 2', 'Răspuns 1', 'Răspuns 2', 'Răspuns 3', [1, 2]],
@@ -46,17 +50,30 @@ function prepare_quiz(optiune) {
   {
     maxim_wrong = 4;
     questions_total = 20;
+    delayMs = 1800000;
+    minute = 30;
+    secunde = 0;
   }
   else if (optiune == 2)
   {
     questions_total = 26;
     maxim_wrong = 5;
+    delayMs = 1800000;
+    minute = 30;
+    secunde = 0;
   }
   else if (optiune == 3)
   {
     questions_total = 5;
     maxim_wrong = 1;
+    delayMs = 10000;
+    minute = 0;
+    secunde = 10;
   }
+
+  contor = setTimeout(function(){ end_quiz("time") }, delayMs);
+  timer();
+  timp = setInterval(function(){ timer(); }, 1000);
 
   questions_remaining = questions_total;
   questions_submitted_arr = [];
@@ -68,12 +85,38 @@ function prepare_quiz(optiune) {
 
   var butoane = document.getElementsByClassName("quiz-prepare");
   butoane[0].style.display = "none";
+  var butoane = document.getElementsByClassName("rezultat");
+  butoane[0].style.display = "none";
   var butoane = document.getElementsByClassName("quiz");
   butoane[0].style.display = "block";
   document.getElementById("RQ").innerHTML = questions_remaining;
   document.getElementById("CQ").innerHTML = questions_corect;
   document.getElementById("WQ").innerHTML = questions_wrong;
   load_question();
+}
+
+function timer()
+{
+  document.getElementById("RT").innerHTML = "";
+  if (minute < 10)
+    document.getElementById("RT").innerHTML += "0" + minute;
+  else
+    document.getElementById("RT").innerHTML += minute;
+
+  document.getElementById("RT").innerHTML += ":";
+
+  if (secunde < 10)
+    document.getElementById("RT").innerHTML += "0" + secunde;
+  else
+    document.getElementById("RT").innerHTML += secunde;
+
+  secunde -= 1;
+  if (secunde < 0)
+    if (minute != 0)
+    {
+      minute -= 1;
+      secunde = 59;
+    }
 }
 
 function mark_question(optiune)
@@ -92,13 +135,34 @@ function mark_question(optiune)
       answers_selected.push(3);
 }
 
-function end_quiz()
+function end_quiz(why)
 {
-  if (questions_wrong >= maxim_wrong)
-    alert("Ai picat cu " + questions_corect + '/' + questions_total + '.');
-  else if (questions_remaining == 0)
-    alert("Ai luat sala cu " + questions_corect + '/' + questions_total + '.');
+  let rezultat = document.getElementsByClassName("rezultat")[0];
+  rezultat.style.display = "block";
+  if (why == "time")
+  {
+    rezultat.style.color = "#940f0f";
+    rezultat.innerHTML = "REZULTAT: Timpul a expirat, ai picat!";
+    alert("Timpul a expirat!");
+  }
+  else {
+    clearTimeout(contor);
+    if (questions_wrong >= maxim_wrong)
+    {
+      rezultat.style.color = "#940f0f";
+      rezultat.innerHTML = "REZULTAT: Ai picat cu " + questions_corect + '/' + questions_total + '.';
+      alert("Ai picat cu " + questions_corect + '/' + questions_total + '.');
 
+    }
+    else if (questions_remaining == 0)
+    {
+      rezultat.style.color = "#0f9442";
+      rezultat.innerHTML = "REZULTAT: Ai luat sala cu " + questions_corect + '/' + questions_total + '.';
+      alert("Ai luat sala cu " + questions_corect + '/' + questions_total + '.');
+    }
+  }
+  clearInterval(timp);
+  clearTimeout(contor);
   var butoane = document.getElementsByClassName("quiz-prepare");
   butoane[0].style.display = "flex";
   var butoane = document.getElementsByClassName("quiz");
@@ -111,6 +175,29 @@ function load_question()
   document.getElementById("RQ").innerHTML = questions_remaining;
   document.getElementById("CQ").innerHTML = questions_corect;
   document.getElementById("WQ").innerHTML = questions_wrong;
+  if (questions[question_index - 1].length == 6)
+  {
+    var mq = window.matchMedia( "(max-width: 768px)" );
+    document.getElementsByClassName("quiz-image")[0].style.display = "block";
+
+    if (mq.matches) {
+      document.getElementsByClassName("quiz-buttons")[0].style.width = "100%";
+      document.getElementsByClassName("quiz-buttons")[0].style.justifyContent = "space-around";
+      document.getElementsByClassName("quiz-image")[0].style.width = "100%";
+      document.getElementsByClassName("quiz-image")[0].style.marginTop = "4%";
+    }
+    else {
+      document.getElementsByClassName("quiz-buttons")[0].style.width = "60%";
+      document.getElementsByClassName("quiz-buttons")[0].style.justifyContent = "flex-start";
+      document.getElementsByClassName("quiz-image")[0].style.width = "30%";
+    }
+    document.getElementById("qimage").src = "images/" + questions[question_index - 1][5];
+  }
+  else {
+    document.getElementsByClassName("quiz-buttons")[0].style.width = "100%";
+    document.getElementsByClassName("quiz-image")[0].style.display = "none";
+    document.getElementById("qimage").src = "";
+  }
   document.getElementById("question").innerHTML = question_index + ". " + questions[question_index - 1][0];
   document.getElementById("buton1").innerHTML = questions[question_index - 1][1];
   document.getElementById("buton2").innerHTML = questions[question_index - 1][2];
@@ -132,11 +219,10 @@ function verify_answers()
       questions_wrong += 1;
       questions_submitted_arr.push(question_index);
     }
-    console.log(questions_remaining);
     if (questions_remaining == 0 || questions_wrong >= maxim_wrong)
     {
       load_question();
-      setTimeout(end_quiz(), 1000);
+      setTimeout(end_quiz(""), 1000);
     }
 
     else
